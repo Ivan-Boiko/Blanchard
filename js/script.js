52e3ce8d7fef05507f25e7a1f078f0833282d400
Your Name <you@example.com>
2021-02-11 22:11:33 +0300

    Landing 3.0

--- js/script.js
+++ js/script.js
@@ -1,26 +1,79 @@
 window.addEventListener('DOMContentLoaded', function(){
-    document.querySelector('#btn-realism').addEventListener('click', function() {
-      document.querySelector('#hidden-menu').classList.toggle('hidden-menu--active');
-      document.querySelector('.header__svg').classList.toggle('header--svg');
+    document.querySelector('.btn-realism').addEventListener('click', function() {
+      document.querySelector('.hidden-menu__realism').classList.toggle('hidden-menu--active');
+      document.querySelector('.hidden-menu__realism').classList.toggle('realism-menu');
     });
+    document.querySelector('.btn-imperesizm').addEventListener('click', function() {
+      document.querySelector('.hidden-menu__imperesizm').classList.toggle('hidden-menu--active');
+      document.querySelector('.hidden-menu__imperesizm').classList.toggle('imperesizm-menu');
+      
+    });
+    document.querySelector('.btn-postimperesizm').addEventListener('click', function() {
+      document.querySelector('.hidden-menu__postimperesizm').classList.toggle('hidden-menu--active');
+      document.querySelector('.hidden-menu__postimperesizm').classList.toggle('postimperesizm-menu');
+      
+    });
+    document.querySelector('.btn-avangard').addEventListener('click', function() {
+      document.querySelector('.hidden-menu__avangard').classList.toggle('hidden-menu--active');
+      document.querySelector('.hidden-menu__avangard').classList.toggle('avangard-menu');
+      
+    });
+    document.querySelector('.btn-footorizm').addEventListener('click', function() {
+      document.querySelector('.hidden-menu__footorizm').classList.toggle('hidden-menu--active');
+      document.querySelector('.hidden-menu__footorizm').classList.toggle('footorizm-menu');
+     
+    });
+  
 
+    document.querySelectorAll('.genrel-menu__btn').forEach(function(tabsGenrelMenu){
+      tabsGenrelMenu.addEventListener('click', function(event){
+        const path = event.currentTarget.dataset.path
+        document.querySelector('.genrel-menu__svg')
+        document.querySelector(`[data-target="${path}"]`).classList.toggle('genrel-menu__svg_rotate');
+      })
+    });
     
+     
     document.querySelector('#events-btn').addEventListener('click', function() {
       document.querySelector('#events_hidden-block1').classList.toggle('events_active');
       document.querySelector('#events_hidden-block2').classList.toggle('events_active');
     })
+    
+    document.querySelectorAll('.katalog__btn').forEach(function(tabsLang){
+      tabsLang.addEventListener('click', function(event){
+        const path = event.currentTarget.dataset.path;
+        document.querySelectorAll('.katalog__subtitle').forEach(function(tabContent){
+          tabContent.classList.remove('katalog_active')
+        })
+        document.querySelector(`[data-target="${path}"]`).classList.add('katalog_active')
+      })
+    });
 
-    // document.querySelectorAll('.katalog__btn').forEach(function(tabsLang){
-    //   tabsLang.addEventListener('click', function(event){
-    //     const path = event.currentTarget.dataset.path
-    //     document.querySelectorAll('.katalog-painter__information').forEach(function(tabContent){
-    //       tabContent.classList.remove('katalog-painter_active')
-    //     })
-    //     document.querySelector('[data-target = "${path}"]').classList.add('katalog-painter_active')
-    //   })
-    // })
 
 
+    document.querySelectorAll('.katalog__btn').forEach(function(tabsLang){
+      tabsLang.addEventListener('click', function(ev){
+        const target = ev.currentTarget.getAttribute('href')
+        document.querySelectorAll('.tab__content').forEach(function(tabContent){
+          tabContent.classList.remove('acardion_active')
+        })
+        document.querySelector(target).classList.add('acardion_active')
+
+        $('.acardion').accordion("refresh");
+      })
+    });
+
+    document.querySelectorAll('.acordion__persons').forEach(function(tabsAcardion){
+      tabsAcardion.addEventListener('click', function(event){
+        const path = event.currentTarget.dataset.path;
+        document.querySelectorAll('.katalog-painter__information').forEach(function(tabContent){
+          tabContent.classList.remove('katalog-painter_active')
+          
+        })
+        document.querySelector(`[data-target="${path}"]`).classList.add('katalog-painter_active')
+      })
+    });
+    
 
     // VALIDATION
 
@@ -63,15 +116,34 @@ window.addEventListener('DOMContentLoaded', function(){
 
 // acardeon
 
-$( "#accordion" ).accordion({
-  collapsible:true,
-  animate: 500
+$( ".acardion" ).accordion({
+  collapsible: true,
+  animate: 300,
+  active:0,
+});
+
+// Свипер для вкладки проэкты
+new Swiper(".gallery__swiper-container", {
+  //Optional parameters
+  navigation: {
+  nextEl: '.gallery__btn-next',
+  prevEl: '.gallery__btn-prev',
+  },
+  pagination:{
+    el: '.gallery__pagination',
+    type: 'fraction',
+  },
+  slidesPerView: 3,
+  spaceBetween: 50,
+  slidesPerColumn: 2,
+  observer:true,
+  observeParents:true,
+  observeSlideChildren:true
 });
 
 
-// Свипер для вкладки проэкты
 
-const mySwiper = new Swiper(".partners-projects__swiper-container", {
+new Swiper(".partners-projects__swiper-container", {
   loop: true,
   effect: 'fade',
   fadeEffect: {
@@ -84,7 +156,7 @@ const mySwiper = new Swiper(".partners-projects__swiper-container", {
 });
 // Свипер для вкладки Издания
 
-const publicationsbookSwiper = new Swiper(".publications-book", {
+new Swiper(".publications-book", {
   loop: true,
   effect: 'fade',
   fadeEffect: {
@@ -102,26 +174,10 @@ const publicationsbookSwiper = new Swiper(".publications-book", {
 
 // Свипер для вкладки Галерея
 
-var gallerySwiper = new Swiper(".gallery__swiper-container", {
-  //Optional parameters
-  loop: true,
-  effect: 'fade',
-  fadeEffect: {
-  crossFade: true
-  },
-  navigation: {
-  nextEl: '.gallery__btn-next',
-  prevEl: '.gallery__btn-prev',
-  },
-  pagination:{
-    el: '.gallery__pagination',
-    type: 'fraction'
-  }
-});
-
+ 
 // Свипер для вкладки hero
 
-var heroSwiper = new Swiper(".swiper-hero", {
+ new Swiper(".swiper-hero", {
   //Optional parameters
   loop: true,
   autoplay: {
@@ -136,7 +192,7 @@ var heroSwiper = new Swiper(".swiper-hero", {
 // инпут для вкладки галерея
 
 const element = document.querySelector('.gallery-menu__select');
-      const choices = new Choices(element, {
+      new Choices(element, {
         itemSelectText: "",
         searchEnabled: false,
         shouldSort: false,
